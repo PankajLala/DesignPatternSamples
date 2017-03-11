bbcec39e7a12a33c93cb473ea7d40519977b2a07
Pankaj Lala <pankaj.lala@xceedance.com>
2017-03-11 13:25:28 +0530

    Simple factory

---
+++ SimpleFactory/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactory
+{
+    /*Simple factory just abstract the responsibility of creating  object and let client delegate it to same.
+     * */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IProduct product = Factory.CreateProduct("X");
+            product.DoWork();
+
+            Console.ReadLine();
+        }
+
+    }
+
+
+    public interface IProduct
+    {
+         void DoWork();
+    }
+
+    public class ProductA : IProduct
+    {
+        void IProduct.DoWork()
+        {
+            Console.WriteLine("Work done by product A"); 
+        }
+
+    }
+
+    public class ProductB: IProduct
+    {
+        void IProduct.DoWork()
+        {
+            Console.WriteLine("Work done by product B");
+        }
+    }
+
+    public  class Factory
+    {
+        public static IProduct CreateProduct(string identifier)
+        {
+            if (identifier == "A")
+                return new ProductA();
+
+            return new ProductB();
+        }
+    }
+}
