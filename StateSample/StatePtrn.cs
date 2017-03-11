461840144ea478a787f764a031473132da6ca9c7
Pankaj Lala <pankaj.lala@xceedance.com>
2017-03-11 13:01:23 +0530

    Statepatern implementation

--- StateSample/StatePtrn.cs
+++ StateSample/StatePtrn.cs
@@ -12,6 +12,64 @@ namespace StateSample
     {
         static void Main(string[] args)
         {
+
+            Context ctx = new Context();
+            ctx.SetState(new StateA());
+
+            ctx.Request();
+            ctx.Request();
+            ctx.Request();
+
+            Console.ReadLine();
+        }
+    }
+
+    class Context
+    {
+        private State _state;
+
+        public void SetState(State state)
+        {
+            _state = state;
+        }
+
+        public void Request()
+        {
+            _state.Handle(this);
+        }
+    }
+
+    abstract class State
+    {
+        public abstract void  Handle(Context ctx);
+    }
+
+    class StateA: State
+    {
+        public override void  Handle(Context cts)
+        {
+            Console.WriteLine("Handled StateA");
+            cts.SetState(new StateB());
+        }
+    }
+
+    class StateB: State
+    { 
+
+        public override void Handle(Context ctx)
+        {
+            Console.WriteLine("Handled StateB");
+            ctx.SetState(new StateC());
+        }
+    }
+
+    class StateC : State
+    {
+
+        public override void Handle(Context ctx)
+        {
+            Console.WriteLine("Handled StateC");
+            Console.WriteLine("Final State");
         }
     }
 }
